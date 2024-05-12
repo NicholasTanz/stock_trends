1e6e2b24bba68388219c39e05de273ccfe7ffed0
E3E <ntanzill@purdue.edu>
2024-05-12 13:56:56 -0400

    tiingo api

--- API_Tiingo.py
+++ API_Tiingo.py
@@ -5,3 +5,28 @@ import config
 import requests
 
 Tiingo_API_Key = config.Tiingo_API_KEY
+
+headers = {
+        'Content-Type': 'application/json',
+        'Authorization' : f'Token {Tiingo_API_Key}'
+        }
+
+baseURL = "https://api.tiingo.com/tiingo/daily/Meta/prices"
+
+def Get_Current_Stock_Price(ticker:str, use_mock_data:bool=False):
+    """ utilizes Tiingo API to return stock open price.
+
+    # NOTE: this utilizes the EOD prices, as intraday requires a subscription. 
+
+    Args:
+        ticker: Stock ticker. (Ex: AAPL, MSFT).
+        use_mock_data: data utilized for testing. 
+
+    Returns:
+        float: Stock open price for given ticker. 
+    """ 
+
+    URL = f"https://api.tiingo.com/tiingo/daily/{ticker}/prices"
+    request = requests.get(URL, headers=headers)
+ 
+    return request.json()[0]['open']
